6e646f983205a4c5f6eaa501a7fed60a1d0e4fa7
Med.Boff <boafiamohamed@gmail.com>
2023-07-13 19:08:25 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %zu byte(s)", sizeof(float));
-	fprintf("Anything\n");
+	fprintf("[Anything]");
 	return (0);
 }
