c38db982c05cbdc68423f62a7373d36bcc2d2d8e
Med-Boff <boafiamohamed@gmail.com>
2023-10-01 15:48:54 -0700

    second task done *-*

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,15 @@
 #include <unistd.h>
 #include "main.h"
 /**
- * _puts_recursion - prints string in reverse
+ * _print_rev_recursion - prints string in reverse
  * @s: input to the fonction
  */
 
-void _puts_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s)
 	{
-		_puts_recursion(s + 1);
+		_print_rev_recursion(s + 1);
 		write(1, s, 1);
 	}
 }
