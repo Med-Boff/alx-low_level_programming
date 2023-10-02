eccaa17bae94cef5edce2501b0e6045556fe2bf3
Med-Boff <boafiamohamed@gmail.com>
2023-10-01 17:32:34 -0700

    Fifth task done *-*

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include <math.h>
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: first input
+ * @y: second input
+ * Return: 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return (pow(2, 3));
+}
