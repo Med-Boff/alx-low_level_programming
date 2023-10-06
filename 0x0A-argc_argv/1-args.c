0c52fd52ba006c35754440fc96a86b35b1f11b96
Med-Boff <boafiamohamed@gmail.com>
2023-10-06 16:03:55 -0700

    Second task done *-*

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of caracters given to this program
+ * @argc: stores the num of argments
+ * @argv: stores the arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)*argv;
+
+	printf("%d\n", argc);
+	return (0);
+}
