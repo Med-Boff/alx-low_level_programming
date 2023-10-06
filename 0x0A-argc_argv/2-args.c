55b46e13b2209f17c3e9dd7f30cb5765909149a3
Med-Boff <boafiamohamed@gmail.com>
2023-10-06 16:42:48 -0700

    Task number 3 done *-*

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints all argumrnts the programe recieves
+ *@argv: stores the arguments
+ *@argc: stores the num of arguments passed to this program
+ *Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int C = 0;
+
+	for (C, C < argc, C++)
+	{
+		printf("%s" argv[C]);
+	}
+	return (0);
+}
