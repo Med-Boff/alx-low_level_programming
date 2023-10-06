10c478f7044fd9edb785c75b7ea400ad1bb1319f
Med-Boff <boafiamohamed@gmail.com>
2023-10-06 15:09:35 -0700

    First task done *-*

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,26 +1,18 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <limits.h>
-#include <stdlib.h>
+
 /**
  * main - fonction that prints its name
  *@argc: variable contains the number of arguments passed to the fonction
  * @argv: an array storing the arguments
- * @cwd: contains the path to the program
  *
  * Return: always 0
  */
 
-int main(int argc, const char *argv[])
+int main(int argc, char *argv[])
 {
-	char cwd[PATH_MAX];
-
 	if (argc)
 	{
-		if (getcwd(cwd, sizeof(cwd)) != NULL)
-		{
-			printf("%s/%s\n", cwd, argv[0]);
-		}
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
