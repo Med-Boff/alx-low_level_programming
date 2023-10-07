2d20a47239fa8029fee715feec0abecaeff4b8c1
Med-Boff <boafiamohamed@gmail.com>
2023-10-06 17:23:34 -0700

    Task number 4 done *-*

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints result of multiplication of two numbers
+ * @argv: stores the arguments passed to this program
+ * @argc: stores the number of arguments
+ *
+ * Return: 0 if successful 1 if not
+ */
+
+int main(int argc, char *argv[])
+{
+	int n1 = atoi(argv[1]);
+	int n2 = atoi(argv[2]);
+	int R = n1 * n2;
+
+	if (argc != 3)
+	{
+		printf("%d\n", R);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
