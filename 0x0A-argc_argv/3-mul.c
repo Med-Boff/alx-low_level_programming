4691e6d2b604f5a05f0def4a106d0f2d93b83104
Med-Boff <boafiamohamed@gmail.com>
2023-11-22 13:37:07 -0800

    fourth task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,20 +5,20 @@
  * @argc: num af args
  * @argv: point to arr of args
  *
- * Return: always 0
+ * Return: 0 if given 2 nums and 1 if not
  */
 int main(int argc, char *argv[])
 {
 	int n = atoi(argv[1]) *  atoi(argv[2]);
 
-	if (argc != 3)
+	if (argc == 3)
 	{
-		printf("Error\n");
+		printf("%d\n", n);
 		return (1);
 	}
 	else
 	{
-		printf("%d\n", n);
+		printf("Error\n");
 	}
 	return (0);
 }
