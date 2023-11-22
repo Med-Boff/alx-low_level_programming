3442b2b13be2496e09f626787fb07c67e7efb8d3
Med-Boff <boafiamohamed@gmail.com>
2023-11-22 08:41:06 -0800

    second task

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - fonction prints numbeer of arguments passed to the program
+ * @argc: contains num of args
+ * @argv: contains args
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
