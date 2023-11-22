1bc403390318ba2059de3d20e13a93cb712ebe24
Med-Boff <boafiamohamed@gmail.com>
2023-11-22 13:26:37 -0800

    fourth task

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,11 +9,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int n1 = atoi(argv[1]); 
-
-	int n2 = atoi(argv[2]);
-
-	int n = n1 * n2;
+	int n = atoi(argv[1]) *  atoi(argv[2]);
 
 	if (argc != 3)
 	{
