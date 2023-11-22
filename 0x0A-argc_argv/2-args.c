a142b39fafb6e17555ef17ddb8e3a1902ef571b6
Med-Boff <boafiamohamed@gmail.com>
2023-11-22 09:03:28 -0800

    third task

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - fonction prints args given to the program
+ * @argc: var contains num of args
+ * @argv: pointer to arr  contains args
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int n = 0;
+
+	while (n < argc)
+	{
+		printf("%s\n", argv[n + 1]);
+		n++;
+	}
+	return (0);
+}
