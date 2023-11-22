aad59a63ef60989b783fce01fad74e0ae680fdd1
Med-Boff <boafiamohamed@gmail.com>
2023-11-22 09:33:33 -0800

    fourth task

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints num
+ * @argc: num af args
+ * @argv: point to arr of args
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int n1 = atoi(argv[1]); 
+
+	int n2 = atoi(argv[2]);
+
+	int n = n1 * n2;
+
+	if (argc != 3)
+	{
+		printf("Error");
+	}
+	else
+	{
+		printf("%d\n", n);
+	}
+	return (0);
+}
