9cb74748c57292438a25d028ddf2be7b15a81eca
Med-Boff <boafiamohamed@gmail.com>
2023-11-30 15:47:16 -0800

    second task

---
+++ 0x0F-function_pointers/1-array_function_pointers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * array_iterator - funtcion executes function as a parameter
+ * @array: an array
+ * @size: var
+ * @action: fonction poiter
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	for (size_t i = 0; i < size; ++i)
+	{
+	action(array[i]);
+	}
+}
