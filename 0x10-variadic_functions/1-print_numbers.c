ce1e2ff1688cd02bc9772af4f36af0dc5b9ca803
Med-Boff <boafiamohamed@gmail.com>
2023-12-01 18:40:06 -0800

    second task

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,10 +13,9 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
+	unsigned int i;
 
 	va_start(args, n);
-
-	unsigned int i;
 
 	for (i = 0; i < n; ++i)
 	{
