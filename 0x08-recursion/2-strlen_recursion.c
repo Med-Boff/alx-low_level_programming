95c1c2bff0f9e8395a36ab8414b7f6281c6b2838
Med-Boff <boafiamohamed@gmail.com>
2023-12-06 16:11:20 -0800

    third task

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,1 +1,20 @@
-#include 
+#include <unistd.h>
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: var holding a string
+ *
+ * Return: number of charachters in the string
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
