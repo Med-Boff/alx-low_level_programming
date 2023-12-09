8b35cccb856ddea04af28820abdbabc37b884203
Med-Boff <boafiamohamed@gmail.com>
2023-12-09 10:54:06 -0800

    third task

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,11 +11,14 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
+	list_t *n_node;
+
 	if (str == NULL)
 	{
 		return (NULL);
 	}
-	list_t *n_node = malloc(sizeof(list_t));
+
+	n_node = malloc(sizeof(list_t));
 
 	if (n_node == NULL)
 	{
