e744c5a4a5b95118a1384a9114bf86c8bc49971e
Med-Boff <boafiamohamed@gmail.com>
2023-12-10 08:08:41 -0800

    task num 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+
+/**
+ * is_prime_number - fonction checks if num is prim
+ * @n: num
+ *
+ * Return: 1 if its prime 0 if not
+ */
+
+int is_prime_number(int n)
+{
+	if (n < 0)
+	{
+		return (0);
+	}
+	else if (n % 2 == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
+
