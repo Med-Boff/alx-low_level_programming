27b98f3a2ab4b5b7bbf2793008b68ae3a1ae4745
Med-Boff <boafiamohamed@gmail.com>
2023-12-09 17:10:27 -0800

    advanced task

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -20,5 +20,5 @@ int main(void)
 
 void b_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n")
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
