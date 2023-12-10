2b95fbab5e54e693aafffa7fe7dc3612395b212a
Med-Boff <boafiamohamed@gmail.com>
2023-12-09 17:06:07 -0800

    advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * b_main - prints string befor main execution
+ *
+ * main - prints string
+ * 
+ * Return: 0
+ */
+
+void b_main(void);
+
+
+int main(void)
+{
+	printf("A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	return (0);
+}
+
+void b_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n")
+}
