561589f65427e061cda9678364180fa874fe00de
Med-Boff <boafiamohamed@gmail.com>
2023-12-12 09:05:06 -0800

    fifth task

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - function that frees a list
+ * @head: pointer to the head of the linked list
+ *
+ */
+
+void free_listint2(listint_t *head)
+{
+	listint_t *t_node = head;
+	listint_t *n_node;
+
+
+
+	while (t_node != NULL)
+	{
+		n_node = t_node->next;
+		free(t_node);
+		t_node = n_node;
+	}
+	head = NULL;
+}
