c833db035fa81b9bd766a5a629dcd2a96c97dfce
Med-Boff <boafiamohamed@gmail.com>
2023-12-13 17:03:39 -0800

    third task

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array and sets it to zero.
+ * @nmemb: number of elements in the array
+ * @size: size in bytes of each element
+ *
+ * Return: a pointer to the allocated memory, or NULL if it fails.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	size_t total_size, i;
+	void *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+	return (NULL);
+	}
+	total_size = nmemb * size;
+
+	ptr = malloc(total_size);
+
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	memset(ptr, 0, total_size);
+
+	return (ptr);
+}
