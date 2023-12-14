471cb0a99b6319fcffc80827763413c41c8ba36a
Med-Boff <boafiamohamed@gmail.com>
2023-12-13 17:30:06 -0800

    task num 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog.
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the newly created dog, or NULL if fails.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+	char *name_copy, *owner_copy;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+	return (NULL);
+	}
+
+	name_copy = strdup(name);
+	if (name_copy == NULL)
+	{
+	free(new_dog);
+	return (NULL);
+	}
+
+	owner_copy = strdup(owner);
+	if (owner_copy == NULL)
+	{
+	free(name_copy);
+	free(new_dog);
+	return (NULL);
+	}
+
+	new_dog->name = name_copy;
+	new_dog->age = age;
+	new_dog->owner = owner_copy;
+
+	return (new_dog);
+}
